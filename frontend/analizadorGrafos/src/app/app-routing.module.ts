6c7f3ca3c477db5ef936e4967aa8d8d05cf9373f
Robin <robinson.1701625954@ucaldas.edu.co>
2022-04-14 10:02:07 -0500

    enrutameinto y creacion de modulos v1

--- frontend/analizadorGrafos/src/app/app-routing.module.ts
+++ frontend/analizadorGrafos/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { LoginUserComponent } from './login-user/login-user.component';
+import { NotFoundComponent } from './template/not-found/not-found.component';
+const routes: Routes = [
+  {
+    path: 'user',
+    component: LoginUserComponent
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/user',
+  },
+  {
+    path:'**',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/analizadorGrafos/src/app/app.module.ts
+++ frontend/analizadorGrafos/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginUserComponent } from './login-user/login-user.component';
+import { HeaderComponent } from './template/header/header.component';
+import { FooterComponent } from './template/footer/footer.component';
+import { NotFoundComponent } from './template/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginUserComponent,
+    HeaderComponent,
+    FooterComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
