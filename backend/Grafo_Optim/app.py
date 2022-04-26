053c9d54931b7d9e01287c5dada39bc6c28f2ab6
Robin <robinson.1701625954@ucaldas.edu.co>
2022-04-26 08:16:40 -0500

    modificacion servidor flask

---
+++ backend/Grafo_Optim/app.py
@@ -0,0 +1,52 @@
+
+#from pymongo import MongoClient
+from flask import Flask, jsonify, request, render_template
+import json 
+import datetime
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+#manejo de errores al traer la informacion
+class JSONEncoder(json.JSONEncoder):
+	def default(self, o):
+		if(isinstance(o, ObjectId)):
+			return str(o)
+		if(isinstance(o, datetime.datetime)):
+			return str(o)	
+		return json.JSONEncoder.default(self, o)
+
+
+app = Flask(__name__)
+
+#conexion con MongoDB
+app.config['MONGO_URI'] = "mongodb+srv://admin:0000@cluster0.8fq1b.mongodb.net/grafo_db?retryWrites=true&w=majority"
+app.json_encoder = JSONEncoder
+mongo = PyMongo(app)
+
+@app.before_request
+def before_request():
+	print("Antes de la peticion...")
+
+@app.after_request
+def after_request(response):
+	print("Despues de la peticion...")
+	return response
+
+@app.route('/')
+def index():
+	return "servidor cliente REST"
+
+
+
+@app.route('/grafo/listar-grafo', methods=['GET'])
+def listar_grafo():
+    if request.method == 'GET':
+        data = mongo.db.grafo.find({})
+        listar_documentos = list(data)
+
+        if data == None:
+            data = []
+        return jsonify({'transaccion': True, "data":listar_documentos})
+
+if __name__ == '__main__':
+    app.run(debug=True, port=7000)
