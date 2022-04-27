a3d3136a08cbfc3878e1e8a966f7a1946949bf36
Robin <robinson.1701625954@ucaldas.edu.co>
2022-04-27 09:25:39 -0500

    servidor funcionando localmente

--- backend/Grafo_Optim/src/app.py
+++ backend/Grafo_Optim/src/app.py
@@ -4,9 +4,10 @@ from flask import Flask, jsonify, request, render_template
 import json 
 import datetime
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
+#from bson.objectid import ObjectId
 
 #manejo de errores al traer la informacion
+'''
 class JSONEncoder(json.JSONEncoder):
 	def default(self, o):
 		if(isinstance(o, ObjectId)):
@@ -15,13 +16,22 @@ class JSONEncoder(json.JSONEncoder):
 			return str(o)	
 		return json.JSONEncoder.default(self, o)
 
-
+'''
 app = Flask(__name__)
 
 #conexion con MongoDB
-app.config['MONGO_URI'] = "mongodb+srv://admin:0000@cluster0.8fq1b.mongodb.net/grafo_db?retryWrites=true&w=majority"
-app.json_encoder = JSONEncoder
+#app.config['MONGO_URI'] = "mongodb+srv://admin:0000@cluster0.8fq1b.mongodb.net/grafo_db?retryWrites=true&w=majority"
+app.config['MONGO_URI'] = "mongodb://localhost:27017/grafo_db"
+#app.json_encoder = JSONEncoder
 mongo = PyMongo(app)
+
+@app.route('/users', methods=['POST'])
+def create_user():
+	#receiving data
+	print(request.json)
+	return {'message':'received'}
+
+"""
 
 @app.before_request
 def before_request():
@@ -32,9 +42,6 @@ def after_request(response):
 	print("Despues de la peticion...")
 	return response
 
-@app.route('/')
-def index():
-	return "servidor cliente REST"
 
 
 
@@ -47,6 +54,6 @@ def listar_grafo():
         if data == None:
             data = []
         return jsonify({'transaccion': True, "data":listar_documentos})
-
+"""
 if __name__ == '__main__':
-    app.run(debug=True, port=7000)
+    app.run(debug=True, port=5000)
