88aae5dde35eabb00c495c1b0dee544867f637b8
Robin <robinson.1701625954@ucaldas.edu.co>
2022-04-27 10:42:08 -0500

    conexion atlas mongo funcionando

--- backend/Grafo_Optim/src/app.py
+++ backend/Grafo_Optim/src/app.py
@@ -4,10 +4,10 @@ from flask import Flask, jsonify, request, render_template
 import json 
 import datetime
 from flask_pymongo import PyMongo
-#from bson.objectid import ObjectId
+from bson.objectid import ObjectId
 
 #manejo de errores al traer la informacion
-'''
+
 class JSONEncoder(json.JSONEncoder):
 	def default(self, o):
 		if(isinstance(o, ObjectId)):
@@ -16,12 +16,14 @@ class JSONEncoder(json.JSONEncoder):
 			return str(o)	
 		return json.JSONEncoder.default(self, o)
 
-'''
+
 app = Flask(__name__)
 
 #conexion con MongoDB
-#app.config['MONGO_URI'] = "mongodb+srv://admin:0000@cluster0.8fq1b.mongodb.net/grafo_db?retryWrites=true&w=majority"
-app.config['MONGO_URI'] = "mongodb://localhost:27017/grafo_db"
+app.config['MONGO_URI'] = "mongodb+srv://admin:0000@cluster0.8fq1b.mongodb.net/grafo_db?retryWrites=true&w=majority"
+app.config['MONGO_DBNAME'] = "grafo_db"
+app.json_encoder = JSONEncoder
+#app.config['MONGO_URI'] = "mongodb://localhost:27017/grafo_db"
 #app.json_encoder = JSONEncoder
 mongo = PyMongo(app)
 
@@ -32,7 +34,7 @@ def create_user():
 	print(request.json)
 	return {'message':'received'}
 
-"""
+
 
 @app.before_request
 def before_request():
@@ -51,10 +53,12 @@ def listar_grafo():
     if request.method == 'GET':
         data = mongo.db.grafo.find({})
         listar_documentos = list(data)
-
         if data == None:
             data = []
         return jsonify({'transaccion': True, "data":listar_documentos})
-"""
+
+
+
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0',port=5000)
