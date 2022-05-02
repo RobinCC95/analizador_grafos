fa5ab15259ccf13166b284a31e37437c88b340d8
Robin <robinson.1701625954@ucaldas.edu.co>
2022-05-02 06:30:35 -0500

    modificacion camponent grafo-view, para obtencion objeto GO para ver cambios en tiempo real- estado en un 50%

--- frontend/analizadorGrafos/src/app/grafo/grafo-view/grafo-view.component.ts
+++ frontend/analizadorGrafos/src/app/grafo/grafo-view/grafo-view.component.ts
@@ -12,19 +12,22 @@ import produce from "immer";
 })
 
 export class GrafoViewComponent implements OnInit {
+
+  @ViewChild('myDiagram', { static: true }) public myDiagramComponent: DiagramComponent;
+  @ViewChild('myPalette', { static: true }) public myPaletteComponent: PaletteComponent;
+
   @Input()
   listNodos = [{}];
   @Input()
   listEdges= [{}];
 
 
-  constructor() {
-   }
+
 
   ngOnInit(): void {
   }
 
-  public state = {
+  /*public state = {
     // Diagram state props
     diagramNodeData: this.listNodos,
     diagramLinkData: this.listEdges,
@@ -38,48 +41,55 @@ export class GrafoViewComponent implements OnInit {
       { key: 'nodoBlue', text: 'nodoBlue', color: 'Blue' }
     ],
     paletteModelData: { prop: 'val' }
+  };*/
+
+  public state = {
+    // Diagram state props
+    diagramNodeData: [
+      { id: 'Alpha', text: "Alpha", color: 'lightblue', loc: "0 0" },
+      { id: 'Beta', text: "Beta", color: 'orange', loc: "100 0" },
+      { id: 'Gamma', text: "Gamma", color: 'lightgreen', loc: "0 100" },
+      { id: 'Delta', text: "Delta", color: 'pink', loc: "100 100" }
+    ],
+    diagramLinkData: [
+        { key: -1, from: 'Alpha', to: 'Beta', fromPort: 'r', toPort: '1' },
+        { key: -2, from: 'Alpha', to: 'Gamma', fromPort: 'b', toPort: 't' },
+        { key: -3, from: 'Beta', to: 'Beta' },
+        { key: -4, from: 'Gamma', to: 'Delta', fromPort: 'r', toPort: 'l' },
+        { key: -5, from: 'Delta', to: 'Alpha', fromPort: 't', toPort: 'r' }
+    ],
+    diagramModelData: { prop: 'value' },
+    skipsDiagramUpdate: false,
+    selectedNodeData: null, // used by InspectorComponent
+
+    // Palette state props
+    paletteNodeData: [
+      { key: 'Epsilon', text: 'Epsilon', color: 'red' },
+      { key: 'Kappa', text: 'Kappa', color: 'purple' }
+    ],
+    paletteModelData: { prop: 'val' }
   };
-
-  public getNodoData() {
-    if (this.listNodos.length <= 1) {
-      this.listNodos = [
-        { id: 'Alpha', text: "Alpha", color: 'lightblue' },
-          { id: 'Beta', text: "Beta", color: 'orange' },
-          { id: 'Omega', text: "Omega", color: 'red' }
-        ];
-      }
-    return this.listNodos;
-  }
-
-  public getEdgesData() {
-    if (this.listEdges.length <= 1) {
-      this.listEdges=[
-        { key: -1, from: 'Alpha', to: 'Beta' },
-        { key: 2, from: 'Alpha', to: 'Omega' }
-      ];
-
-    }
-    return this.listEdges;
-  }
 
   public diagramDivClassName: string = 'myDiagramDiv';
   public paletteDivClassName = 'myPaletteDiv';
 
-  // initialize diagram / templates
-public initDiagram(): go.Diagram {
-  const $ = go.GraphObject.make;
-  const dia = $(go.Diagram, {
-    'undoManager.isEnabled': true,
-    model: new go.GraphLinksModel(
-      {
-        nodeKeyProperty: 'id',
-        linkToPortIdProperty: 'toPort',
-        linkFromPortIdProperty: 'fromPort',
-        linkKeyProperty: 'key' // IMPORTANT! must be defined for merges and data sync when using GraphLinksModel
-      }
-    )
-  });
-  dia.commandHandler.archetypeGroupData = { key: 'Group', isGroup: true };
+  public initDiagram(): go.Diagram {
+
+    const $ = go.GraphObject.make;
+    const dia = $(go.Diagram, {
+      'undoManager.isEnabled': true,
+      'clickCreatingTool.archetypeNodeData': { text: 'new node', color: 'lightblue' },
+      model: $(go.GraphLinksModel,
+        {
+          nodeKeyProperty: 'id',
+          linkToPortIdProperty: 'toPort',
+          linkFromPortIdProperty: 'fromPort',
+          linkKeyProperty: 'key' // IMPORTANT! must be defined for merges and data sync when using GraphLinksModel
+        }
+      )
+    });
+
+    dia.commandHandler.archetypeGroupData = { key: 'Group', isGroup: true };
 
     const makePort = function(id: string, spot: go.Spot) {
       return $(go.Shape, 'Circle',
@@ -92,8 +102,8 @@ public initDiagram(): go.Diagram {
       );
     }
 
-  // define the Node template
-  dia.nodeTemplate =
+    // define the Node template
+    dia.nodeTemplate =
       $(go.Node, 'Spot',
         {
           contextMenu:
@@ -127,46 +137,114 @@ public initDiagram(): go.Diagram {
     return dia;
   }
 
-/**
- * Handle GoJS model changes, which output an object of data changes via Mode.toIncrementalData.
- * This method should iterate over thoe changes and update state to keep in sync with the FoJS model.
- * This can be done with any preferred state management method, as long as immutability is preserved.
- */
-public diagramModelChange = function(changes: go.IncrementalData) {
-  console.log(changes);
+  // When the diagram model changes, update app data to reflect those changes. Be sure to use immer's "produce" function to preserve immutability
+  public diagramModelChange = function(changes: go.IncrementalData) {
+    console.log('diagramModelChange');
+    if (!changes) return;
 
-  // see gojs-angular-basic for an example model changed handler that preserves immutability
-  // when setting state, be sure to set skipsDiagramUpdate: true since GoJS already has this update
-};
+    /*const appComp = this;
+    this.state = produce(this.state, draft => {
+      // set skipsDiagramUpdate: true since GoJS already has this update
+      // this way, we don't log an unneeded transaction in the Diagram's undoManager history
+      draft.skipsDiagramUpdate = true;
+      draft.diagramNodeData = DataSyncService.syncNodeData(changes, draft.diagramNodeData, appComp.observedDiagram.model);
+      draft.diagramLinkData = DataSyncService.syncLinkData(changes, draft.diagramLinkData, appComp.observedDiagram.model);
+      draft.diagramModelData = DataSyncService.syncModelData(changes, draft.diagramModelData);
+      // If one of the modified nodes was the selected node used by the inspector, update the inspector selectedNodeData object
+      const modifiedNodeDatas = changes.modifiedNodeData;
+      if (modifiedNodeDatas && draft.selectedNodeData) {
+        for (let i = 0; i < modifiedNodeDatas.length; i++) {
+          const mn = modifiedNodeDatas[i];
+          const nodeKeyProperty = appComp.myDiagramComponent.diagram.model.nodeKeyProperty as string;
+          if (mn[nodeKeyProperty] === draft.selectedNodeData[nodeKeyProperty]) {
+            draft.selectedNodeData = mn;
+          }
+        }
+      }
+    });*/
+  };
 
-public reinitModel(){
-  console.log('presiono');
-}
+  public initPalette(): go.Palette {
+    const $ = go.GraphObject.make;
+    const palette = $(go.Palette);
+
+    // define the Node template
+    palette.nodeTemplate =
+      $(go.Node, 'Auto',
+        $(go.Shape, 'RoundedRectangle',
+          {
+            stroke: null
+          },
+          new go.Binding('fill', 'color')
+        ),
+        $(go.TextBlock, { margin: 8 },
+          new go.Binding('text', 'key'))
+      );
+
+    palette.model = $(go.GraphLinksModel);
+    return palette;
+  }
+
+  constructor(private cdr: ChangeDetectorRef) { }
+
+  // Overview Component testing
+  public oDivClassName = 'myOverviewDiv';
+  public initOverview(): go.Overview {
+    const $ = go.GraphObject.make;
+    const overview = $(go.Overview);
+    return overview;
+  }
+
+  public observedDiagram = null;
+
+  // currently selected node; for inspector
+  public selectedNodeData: go.ObjectData ;
+
+  /*public ngAfterViewInit() {
+    if (this.observedDiagram) return;
+    //this.observedDiagram = this.myDiagramComponent.diagram;
+    this.cdr.detectChanges(); // IMPORTANT: without this, Angular will throw ExpressionChangedAfterItHasBeenCheckedError (dev mode only)
+
+    //const appComp: AppComponent = this;
+    // listener for inspector
+    this.myDiagramComponent.diagram.addDiagramListener('ChangedSelection', function(e) {
+      if (e.diagram.selection.count === 0) {
+        appComp.selectedNodeData = null;
+      }
+      const node = e.diagram.selection.first();
+      appComp.state = produce(appComp.state, draft => {
+        if (node instanceof go.Node) {
+          var idx = draft.diagramNodeData.findIndex(nd => nd.id == node.data.id);
+          var nd = draft.diagramNodeData[idx];
+          draft.selectedNodeData = nd;
+        } else {
+          draft.selectedNodeData = null;
+        }
+      });
+    });
+  } // end ngAfterViewInit  */
 
 
-public initPalette(): go.Palette {
-  const $ = go.GraphObject.make;
-  const palette = $(go.Palette);
+  /**
+   * Update a node's data based on some change to an inspector row's input
+   * @param changedPropAndVal An object with 2 entries: "prop" (the node data prop changed), and "newVal" (the value the user entered in the inspector <input>)
+   */
+  /*public handleInspectorChange(changedPropAndVal) {
 
-  // define the Node template
-  palette.nodeTemplate =
-    $(go.Node, 'Auto',
-      $(go.Shape, 'RoundedRectangle',
-        {
-          stroke: null
-        },
-        new go.Binding('fill', 'color')
-      ),
-      $(go.TextBlock, { margin: 8 },
-        new go.Binding('text', 'key'))
-    );
+    const path = changedPropAndVal.prop;
+    const value = changedPropAndVal.newVal;
 
-  palette.model = $(go.GraphLinksModel);
-  return palette;
-}
-
-
-
+    this.state = produce(this.state, draft => {
+      var data = draft.selectedNodeData;
+      data[path] = value;
+      const key = data.id;
+      const idx = draft.diagramNodeData.findIndex(nd => nd.id == key);
+      if (idx >= 0) {
+        draft.diagramNodeData[idx] = data;
+        draft.skipsDiagramUpdate = false; // we need to sync GoJS data with this new app state, so do not skips Diagram update
+      }
+    });
+  }*/
 
 
 }
